2bd3c005ffda91d2f25b2babb984b6fd39e615d0
Mathieu Gaydon <154428878+MathieuGaydon@users.noreply.github.com>
2026-05-18 09:08:27 +0000

    feat(exercice2): termine l'exercice

--- src/main/java/fr/univ_amu/iut/exercice2/CompteurController.java
+++ src/main/java/fr/univ_amu/iut/exercice2/CompteurController.java
@@ -1,5 +1,6 @@
 package fr.univ_amu.iut.exercice2;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
@@ -49,6 +50,7 @@ public class CompteurController {
     //
     // Ainsi, chaque fois que la valeur de `compteur` change (via incrementer/decrementer/reinit),
     // le label se met à jour automatiquement - aucun setText() à appeler !
+    labelCompteur.textProperty().bind(Bindings.convert(compteur));
   }
 
   /**
@@ -60,18 +62,24 @@ public class CompteurController {
   @FXML
   private void incrementer() {
     // TODO exercice 2 : incrémenter la valeur de la propriété compteur.
+    compteur.get();
+    compteur.set(getCompteur() + 1);
   }
 
   /** Action du bouton −. Décrémente le compteur. */
   @FXML
   private void decrementer() {
     // TODO exercice 2 : décrémenter la valeur de la propriété compteur.
+    compteur.get();
+    compteur.set(getCompteur() - 1);
   }
 
   /** Action du bouton Réinitialiser. Remet le compteur à zéro. */
   @FXML
   private void reinitialiser() {
     // TODO exercice 2 : remettre la propriété compteur à 0.
+    compteur.get();
+    compteur.set(0);
   }
 
   /**
